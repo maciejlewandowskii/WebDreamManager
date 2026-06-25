c518f6436f89ad04c28df5e5f7b04b3679731e3c
dev <maciej.lewandowski@mite.pl>
2026-06-25 17:50:36 +0200

    Add system setting to toggle company address visibility on PDFs

--- src/Infrastructure/Pdf/DocumentPdfGenerator.php
+++ src/Infrastructure/Pdf/DocumentPdfGenerator.php
@@ -6,6 +6,7 @@ namespace App\Infrastructure\Pdf;
 
 use App\Domain\Invoicing\Entity\Invoice;
 use App\Domain\Invoicing\Entity\Quote;
+use App\Domain\System\Repository\SystemSettingRepositoryInterface;
 use Dompdf\Dompdf;
 use Dompdf\Options;
 use RuntimeException;
@@ -16,6 +17,7 @@ final class DocumentPdfGenerator
 {
     public function __construct(
         private readonly Environment $twig,
+        private readonly SystemSettingRepositoryInterface $settings,
         #[Autowire('%kernel.project_dir%/var/project_files')] private readonly string $projectFilesDir,
         #[Autowire('%kernel.project_dir%/var/customer_files')] private readonly string $customerFilesDir,
     ) {}
@@ -23,8 +25,9 @@ final class DocumentPdfGenerator
     public function generateForInvoice(Invoice $invoice, string $colorMode): GeneratedPdf
     {
         $html = $this->twig->render('pdf/invoice.html.twig', [
-            'invoice'   => $invoice,
-            'colorMode' => $colorMode,
+            'invoice'              => $invoice,
+            'colorMode'            => $colorMode,
+            'show_company_address' => $this->settings->get('PDF_SHOW_COMPANY_ADDRESS', '1') !== '0',
         ]);
 
         $fileName  = sprintf('invoice-%s-%s.pdf', $this->safeNumber($invoice->getNumber()), date('Ymd-His'));
@@ -39,8 +42,9 @@ final class DocumentPdfGenerator
     public function generateForQuote(Quote $quote, string $colorMode): GeneratedPdf
     {
         $html = $this->twig->render('pdf/quote.html.twig', [
-            'quote'     => $quote,
-            'colorMode' => $colorMode,
+            'quote'                => $quote,
+            'colorMode'            => $colorMode,
+            'show_company_address' => $this->settings->get('PDF_SHOW_COMPANY_ADDRESS', '1') !== '0',
         ]);
 
         $fileName  = sprintf('quote-%s-%s.pdf', $this->safeNumber($quote->getNumber()), date('Ymd-His'));
